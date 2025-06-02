89b2cb9611d41cf0853069af6ab448222be58c20
montse-guerra <montseguerra009@gmail.com>
2025-06-02 00:31:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="planta.png">
+        <title> Plantas Endemicas </title>
+        <style>
+            ul{
+                list-style-type:none;
+                margin:0;
+                padding:0;
+                overflow:hidden;
+                background-color:green;
+            }
+            li{
+                float:left;
+            }
+            li a{
+                display:black;
+                color:white;
+                text-align:center;
+                padding:15px 40px;
+                text-decoration:none;
+            }
+            li a:hover{
+                background-color:#111;
+            }
+        </style>
+    </head>
+</html>
+<body background="fondo madera.jpg">
+    <center>
+        <table width="80%" border="0" cellpadding="30px" bgcolor="Beige">
+        <tr>
+            <td>
+            <font face="Cooper Black" color="green"><h1> Plantas Endemicas de Yucatán </h1></font> 
+                <p> </p>
+                <center> <hr width="90%"> </center>
+                <br>
+                <section id="menu">
+                <h2> 5 plantas endemicas </h2>
+                <p>
+                    <ul>
+                        <li><a href="#balche">Balche</a></li>
+                        <li><a href="#Chaka">Chaka</a></li>
+                        <li><a href="#Jabin">jabin</a></li>
+                        <li><a href="#Pich">Pich</a></li>
+                        <li><a href="#Makulis">Makulis rosa</a></li>
+                    </ul>
+                    <center><hr width="90%"></center>
+                    <p></p>
+                    <section id="balche">
+                    <font face="Elephant" color="coral"><h2>Balche</h2></font>
+                    </section>
+                    <p>
+                        <img src="balche.jpg">
+                        <p>
+                            El árbol, llamado Lonchocarpus longistylus, es endémico de la Península de Yucatán y es considerado sagrado por los mayas.
+                            <p>
+                    <a href="#menu"><img src="regresar.png" alt="regresar"></a><p></p>
+                    <center><hr width="90%"></center>
+                    <p></p>
+                    <section id="Chaka">
+                    <font face="Elephant" color="coral"><h2>Chaka</h2></font>
+                    </section>
+                    <p>
+                        <img src="chak.jpg">
+                        <p>
+                            Árbol de hasta 15 mts. de altura, con el tronco recto, escamoso y muy ramificado en la copa; las flores son pequeñas de color crema-verdoso muy aromática, florea de febrero a mayo; los frutos son cápsulas drupáceas de 1 a 1.5 cm., es una especie caducifolia.
+                            <p>
+                    <a href="#menu"><img src="regresar.png" alt="regresar"></a><p></p>
+                    <center><hr width="90%"></center>
+                    <p></p>
+                    <section id="Jabin">
+                    <font face="Elephant" color="coral"><h2>Jabin</h2></font>
+                    </section>
+                    <p>
+                        <img src="jab.jpg">
+                        <p>
+                            Árbol que alcanza hasta 20 m. de altura, caducifolio, copa densa, corteza fisurada, hojas ovadas compuestas imparipinnadas, foliolos elípticos verde oscuros, flores en panículas ligeramente perfumadas, pétalos rosados o ligeramente morados florea de febrero a mayo, frutos en forma de vaina con alas de color café y alargados quebradizos al madurar.
+                            <p>
+                    <a href="#menu"><img src="regresar.png" alt="regresar"></a><p></p>
+                    <center><hr width="90%"></center>
+                    <p></p>
+                    <section id="Pich">
+                    <font face="Elephant" color="coral"><h2>Pich</h2></font>
+                    </section>
+                    <p>
+                        <img src="pich.jpg">
+                        <p>
+                            Árbol grande y llamativo, caducifolio, de 20 a 30 m. de altura, follaje abundante, hojas bipinnadas los foliolos se pliegan en la noche, ramas ascendentes, corteza lisa a granulosa gris clara con abundantes lenticelas alargadas; flores en pequeñas cabezuelas pedunculadas actinomórficas, caliz verde y tubular, corola verde clara, florece de marzo a mayo; el fruto es una vaina circular indehiscente de 7 a 15 cm.
+                            <p>
+                    <a href="#menu"><img src="regresar.png" alt="regresar"></a><p></p>
+                    <center><hr width="90%"></center>
+                    <p></p>
+                    <section id="Makulis">
+                    <font face="Elephant" color="coral"><h2>Makulis rosa</h2></font>
+                    </section>
+                    <p>
+                        <img src="makulis.jpg">
+                        <p>
+                            Árbol de hasta 15 mts. de altura, el tronco es recto y fisurado y la copa piramidal; las flores son de color rosa-morado, muy vistosas, dispuestas al final de las ramas; los frutos son cápsulas de 30 a 40 cms. de largo, ligeramente retorcidos.
+                             <p>
+                    <a href="#menu"><img src="regresar.png" alt="regresar"></a><p>
+            </td>
+        </tr>
+        </table>
+    </center>
+</body>
+</html>
